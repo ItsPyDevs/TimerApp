7a6495bdfd97871f47ddc40c551738d59a42e4ef
ItsPyDevs <128306684+ItsPyDevs@users.noreply.github.com>
2024-06-02 16:28:51 +0200

    Add files via upload

---
+++ TimerApp/TimerApp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TimerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(201, 3);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(95, 29);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 35);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(326, 82);
+            this.MinimumSize = new System.Drawing.Size(326, 82);
+            this.Name = "Form1";
+            this.Text = "TimerApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ TimerApp/TimerApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace TimerApp
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                Thread thread = new Thread(new ThreadStart(Currenttime));
+                thread.Start();
+            } catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void Currenttime()
+        {
+            while (true)
+            {
+                try {
+                    Invoke(new Action(() =>
+                    {
+                        string dt = DateTime.Now.ToString("HH:mm:ss");
+                        label1.Text = $"Time: {dt}"; 
+                    }
+                    )
+                        );
+                } catch { return; }
+            }
+        }
+
+
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ItsPyDevs");
+        }
+    }
+}
